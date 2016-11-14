b9651deb1e951703ec6293fb65613d3752341629
Vincent Wei HU <huweihugh@gmail.com>
2016-11-14 20:08:04 +0800

    Add files via upload

---
+++ SVM/SVM.m
@@ -0,0 +1,59 @@
+%June11,2016
+%SVM
+function [alpha,Ker,beta0]=SVM(X,Y,kernel)
+% X is N*p, Y is N*1,{-1,1}
+% Constant=Inf for Hard Margin
+global  precision Cost
+
+switch kernel
+    case 'linear'
+        Ker=Ker_Linear(X,X);
+    case 'ploynomial'
+        Ker=Ker_Polynomial(X,X);
+    case 'RBF'
+        Ker=Ker_RBF(X,X);
+    case 'NeutralNet'
+        Ker=Ker_NeutralNet(X,X);
+end
+
+H=outproduction(Y,Y).*Ker;
+f=repmat(-1,size(X,1),1);
+Aeq=Y';
+beq=0;
+A_1=diag(ones(size(X,1),1));
+A_2=diag(-1*ones(size(X,1),1));
+A=[A_1
+    A_2];
+b_1=repmat(Cost,size(X,1),1);
+b_2=zeros(size(X,1),1);
+
+b=[b_1
+    b_2];
+alpha=quadprog(H,f,A,b,Aeq,beq);
+
+
+
+serial_num=(1:size(X,1))';
+serial_sv=serial_num(alpha>precision&alpha<Cost);
+
+temp_beta0=0;
+for i=1:size(serial_sv,1)
+    temp_beta0=temp_beta0+Y(serial_sv(i));
+    temp_beta0=temp_beta0-sum(alpha(serial_sv(i))*...
+        Y(serial_sv(i))*Ker(serial_sv,serial_sv(i)));
+end
+beta0=temp_beta0/size(serial_sv,1);
+
+
+
+return
+
+
+
+
+
+
+
+
+
+
---
+++ SVM/SVM_plot.m
@@ -0,0 +1,38 @@
+function SVM_plot(X,Y,alpha,beta0,kernel)
+% X is with 2 coloums
+
+global Cost
+figure
+hold on
+for i=1:size(Y,1)
+    if Y(i)==1
+        plot(X(i,1),X(i,2),'ro');
+    elseif Y(i)==-1
+        plot(X(i,1),X(i,2),'bx');
+    end
+end
+%linear
+xvar=linspace(min(X(:,1)),max(X(:,1)),100);
+switch kernel
+    case 'linear'
+        beta=X'*(alpha.*Y);
+        yvar=(-beta0-xvar*beta(1))/beta(2);
+        plot(xvar,yvar,'-');
+        xlabel('X1');ylabel('X2');title(strcat('c=',num2str(Cost)));
+    case 'polynomial'
+        fun=@(X)Ker_Polynomial([xvar,X],[xvar,X])*(alpha.*Y)+beta0;
+        X0 = zeros(size(xvar));
+        yvar = fsolve(fun,X0);
+        plot(xvar,yvar,'-');
+        xlabel('X1');ylabel('X2');title(strcat('c=',num2str(Cost)));
+    case 'RBF'
+        
+        fun=@(X)(Ker_RBF([xvar,X],[xvar,X]))*(alpha.*Y)+beta0;
+        X0 = zeros(size(xvar));
+        yvar = fsolve(fun,X0);
+        plot(xvar,yvar,'-');
+        xlabel('X1');ylabel('X2');title(strcat('c=',num2str(Cost)));
+end
+
+hold off
+return
